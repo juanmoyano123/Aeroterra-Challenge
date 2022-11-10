463058b0de3357885d6f7e38216d6832474d0502
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-11-10 13:19:10 -0300

    point and popuptemplate

--- main.js
+++ main.js
@@ -4,44 +4,65 @@ require([
   "esri/views/MapView",
   "esri/Graphic",
   "esri/layers/GraphicsLayer",
-  "esri/widgets/Search"
-], function (esriConfig, Map, MapView, Graphic, GraphicsLayer,Search ) {
+], function (esriConfig, Map, MapView, Graphic, GraphicsLayer) {
   esriConfig.apiKey =
     "AAPKe975aa7445864baf9bb81bfaa913ac96fJu-zNda6AM4SXOQZNQRBqtL6CniLD8HnmA0AGrIAZ_x0P2Saw6XrAoUNXL8UbiG";
 
   const map = new Map({
-    basemap: "arcgis-navigation"
+    basemap: "arcgis-topographic", // Basemap layer service
   });
 
   const view = new MapView({
-    container: "viewDiv",
     map: map,
-    center: [-122.3321,47.6062],
-    zoom: 12
+    center: [-158.805, 34.027], //Longitude, latitude
+    zoom: 13,
+    container: "viewDiv",
   });
 
-  const search = new Search({  //Add Search widget
-    view: view
+  const graphicsLayer = new GraphicsLayer();
+  map.add(graphicsLayer);
+
+  //Atributos de los puntos-----------------------------------
+
+  const simpleMarkerSymbol = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // White
+      width: 1,
+    },
+  };
+
+  //Coordenadas de los puntos-----------------------------------
+
+  const point = {
+    type: "point",
+    longitude: -158.80657463861,
+    latitude: 34.0005930608889,
+  };
+
+  //PopUp Template de los puntos-----------------------------------
+
+  const popupTemplate = {
+    title: "{Name}",
+    content: "{Description}",
+  };
+  const attributes = {
+    Name: "Graphic",
+    Description: "I am a polygon",
+  };
+
+  //Render del punto + Popup  de los puntos-----------------------------------
+
+  const pointGraphic = new Graphic({
+    geometry: point,
+    symbol: simpleMarkerSymbol,
+    attributes: attributes,
+    popupTemplate: popupTemplate,
   });
 
-  view.ui.add(search, "top-right"); //Add to the map
-
+  graphicsLayer.add(pointGraphic);
 });
-
-
-
-
-let handleSubmit = document.querySelector("#handleSubmit");
-handleSubmit.addEventListener("click", addToPoiList);
-function addToPoiList(e) {
-  e.preventDefault();
-  let desc = document.querySelector("#description").value;
-  let adr = document.querySelector("#adress").value;
-  let pho = document.querySelector("#phone");
-  let coord = document.querySelector("#coordinates").value.split(",");
-  let typ = document.querySelector("#type").value;
-  poiConstructor(desc, adr, pho, coord, typ);
-}
 
 let poiList = [];
 function poiConstructor(description, adress, phone, coordinates, type) {
@@ -58,41 +79,42 @@ function poiConstructor(description, adress, phone, coordinates, type) {
   console.log(poiList);
 }
 
+let handleSubmit = document.querySelector("#handleSubmit");
+handleSubmit.addEventListener("click", addToPoiList());
+function addToPoiList(e) {
+  e.preventDefault();
+  let desc = document.querySelector("#description").value;
+  let adr = document.querySelector("#adress").value;
+  let pho = document.querySelector("#phone");
+  let point = document.querySelector("#coordinates").value.split(",");
+  let typ = document.querySelector("#type").value;
+  poiConstructor(desc, adr, pho, point, typ);
+}
+
 /* 
-  //POINT-------------------------------------------------------------------------
+const point = {
+  type: "point",
+  longitude: -158.80657463861,
+  latitude: 34.0005930608889,
+};
 
-  const point = {
-    //Create a point
-    type: "point",
-    longitude: -118.80657463861,
-    latitude: 34.0005930608889,
+//PopUp Template de los puntos-----------------------------------
+
+  const popupTemplate = {
+    title: "{Name}",
+    content: "{Description}",
+  };
+  const attributes = {
+    Name: "Graphic",
+    Description: "I am a polygon",
   };
 
-  const simpleMarkerSymbol = {
-    type: "simple-marker",
-    color: [226, 119, 40], // Orange
-    outline: {
-      color: [255, 255, 255], // White
-      width: 1,
-    },
-  };
-  const pointGraphic = new Graphic({
-    geometry: point,
-    symbol: simpleMarkerSymbol,
-  });
-  graphicsLayer.add(pointGraphic);
+//Render del punto + Popup  de los puntos-----------------------------------
 
-  //PopUp-------------------------------------------------------------------------
-
-  const popupTrailheads = {
-    title: "Trailhead",
-    content:
-      "<b>Trail:</b> {TRL_NAME}<br><b>City:</b> {CITY_JUR}<br><b>Cross Street:</b> {X_STREET}<br><b>Parking:</b> {PARKING}<br><b>Elevation:</b> {ELEV_FT} ft",
-  };
-  const trailheads = new FeatureLayer({
-    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0",
-    outFields: ["TRL_NAME", "CITY_JUR", "X_STREET", "PARKING", "ELEV_FT"],
-    popupTemplate: popupTrailheads,
-  });
-
-  map.add(trailheads); */
+const pointGraphic = new Graphic({
+  geometry: point,
+  symbol: simpleMarkerSymbol,
+  attributes: attributes,
+  popupTemplate: popupTemplate,
+});
+ */
