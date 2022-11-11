d1ffaf932bd763ef4fbef872e8b71f8fdd1b4042
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-11-11 12:39:28 -0300

    optimization

--- main.js
+++ main.js
@@ -15,10 +15,15 @@ function addToPoiList(e) {
   let pho = document.querySelector("#phone").value;
   let coordinates = document.querySelector("#coordinates").value.split(",");
   let typ = document.querySelector("#type").value;
-  //poiConstructor(desc, adr, pho, coordinates, typ);
+  poiConstructor(desc, adr, pho, coordinates, typ);
   let point = [coordinates[0], coordinates[1]];
   map.flyTo(point, 13);
-  L.marker(point).addTo(map).bindPopup(`${desc}`).openPopup();
+  L.marker(point)
+    .addTo(map)
+    .bindPopup(
+      `Description: ${desc}<br> Adress: ${adr}<br> Phone:${pho}<br> Coordinates:${coordinates}`
+    )
+    .openPopup();
 }
 
 let deleteButton = document.getElementById("handleDelete");
@@ -26,7 +31,7 @@ const deleteFunction = () => {
   localStorage.clear("polist");
 };
 
-/* let poiList = [];
+let poiList = [];
 function poiConstructor(description, adress, phone, coordinates, type) {
   let poi = {
     pdescription: description,
@@ -35,8 +40,35 @@ function poiConstructor(description, adress, phone, coordinates, type) {
     pcoordinates: coordinates,
     ptype: type,
   };
+
   console.log(poi);
   poiList.push(poi);
+  console.log(poiList);
   localStorage.setItem("poiList", JSON.stringify(poiList));
+  /*renderPointsMap(poiList);*/
+  renderListPoi(poiList);
 }
- */
+
+/* function renderPointsMap(array) {
+  array.map((e) => {
+    map.flyTo(e.pcoordinates, 13);
+    L.marker(e.pcoordinates)
+      .addTo(map)
+      .bindPopup(
+        `Description: ${e.pdescription}<br> Adress: ${e.padress}<br>Coordinates: ${e.pcoordinates} Phone:${e.pphone}`
+      )
+      .openPopup();
+  });
+} */
+
+function renderListPoi(array) {
+  let poiItemList = document.getElementById("poiItemList");
+  poiItemList.innerHTML = "";
+  array.map((e) => {
+    let liPoi = document.createElement("ul");
+    liPoi.className = "list";
+    liPoi.innerHTML = "";
+    liPoi.innerHTML = `<li>Description: ${e.pdescription}<br> Adress: ${e.padress}<br>Coordinates: (${e.pcoordinates}) <br>Phone:${e.pphone}</li>`;
+    poiItemList.appendChild(liPoi);
+  });
+}
